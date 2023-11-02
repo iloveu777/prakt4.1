1b9daab900d6a5487dd26da35239c3d3d77e73d8
iloveu777 <zuxplil1337@yandex.ru>
2023-11-02 08:47:14 +0200

    asd

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BackgroundColor = ConsoleColor.White;
+            ForegroundColor = ConsoleColor.Black;
+            WindowHeight = 60;
+            WindowWidth = 25;
+            Title = "Тема #4, КП # 1. Ривилис Евгений";
+            Write("Как Вас зовут? ");
+            string name = ReadLine();
+            WriteLine($"Здравствуйте {name}!");
+            WriteLine(CapsLock);
+            WriteLine(NumberLock);
+
+            ReadKey();
+
+        }
+    }
+}
